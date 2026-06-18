ede7cdac76ef3f40455bbd0ba04abe863d27f5d8
Kai-svg-97 <kai20231202@gmail.com>
2026-06-18 15:19:30 +0800

    fix: bundle Noto Sans KR for Korean text rendering on Linux/WSL

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
+import "@fontsource/noto-sans-kr/400.css";
 import "@fontsource/fira-code/400.css";
 import "@fontsource/fira-code/500.css";
 import App from "./App";
